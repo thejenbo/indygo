b7d75b9316a3ca039347d48f875af218dc6ea4d5
thejenbo <jbormacoff@gmail.com>
2016-02-06 15:15:16 -0800

    initial files

---
+++ public/src/js/modules.js
@@ -0,0 +1,8 @@
+var $ = require('jquery-browserify');
+require('jquery');
+require('angular');
+require('angular-animate');
+require('angular-sanitize');
+require('angular-ui-router');
+require('imagesloaded');
+require('ngstorage');
